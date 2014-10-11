9ae21ad0e41d1960039979250a941e8f253ce206
Peter Markou <markoupetr@gmail.com>
2014-10-11 13:03:04 +0300

    When the user changes the state of a TreeNode to Unchecked, change the state of the subtree to Unchecked as well.

--- DEiXTo/Presenters/DeixtoAgentPresenter.cs
+++ DEiXTo/Presenters/DeixtoAgentPresenter.cs
@@ -59,36 +59,23 @@ namespace DEiXTo.Presenters
 
         void nodeStateChanged(TreeNode node, NodeState state)
         {
-            int imageIndex = -1;
-            
-
-            switch (state)
-            {
-                case NodeState.Checked:
-                    imageIndex = 0;
-                    break;
-                case NodeState.CheckedImplied:
-                    imageIndex = 1;
-                    break;
-                case NodeState.CheckedSource:
-                    imageIndex = 2;
-                    break;
-                case NodeState.Grayed:
-                    imageIndex = 3;
-                    break;
-                case NodeState.GrayedImplied:
-                    imageIndex = 4;
-                    break;
-                case NodeState.Unchecked:
-                    imageIndex = 5;
-                    break;
-                default:
-                    imageIndex = -1;
-                    break;
-            }
-
+            int imageIndex = new StateToImageMapper().GetImageFromState(state);
             _view.ApplyStateToNode(node, imageIndex);
             node.SetState(state);
+
+            if (imageIndex == 5)
+            {
+                applyUncheckedToSubtree(node.Nodes);
+            }
+        }
+
+        private void applyUncheckedToSubtree(TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                _view.ApplyStateToNode(node, 5);
+                applyUncheckedToSubtree(node.Nodes);
+            }
         }
 
         void levelDownWorkingPattern(TreeNode node)
---
+++ DEiXTo/Services/StateToImageMapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEiXTo.Services
+{
+    public class StateToImageMapper
+    {
+        public int GetImageFromState(NodeState state)
+        {
+            int imageIndex = -1;
+
+            switch (state)
+            {
+                case NodeState.Checked:
+                    imageIndex = 0;
+                    break;
+                case NodeState.CheckedImplied:
+                    imageIndex = 1;
+                    break;
+                case NodeState.CheckedSource:
+                    imageIndex = 2;
+                    break;
+                case NodeState.Grayed:
+                    imageIndex = 3;
+                    break;
+                case NodeState.GrayedImplied:
+                    imageIndex = 4;
+                    break;
+                case NodeState.Unchecked:
+                    imageIndex = 5;
+                    break;
+                default:
+                    imageIndex = -1;
+                    break;
+            }
+
+            return imageIndex;
+        }
+    }
+}
