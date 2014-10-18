48e918cf058f4a9e5ceb7224ea8cdbbd5b47d5c2
Peter Markou <markoupetr@gmail.com>
2014-10-18 12:33:33 +0300

    Combine the Underline and Bold FontStyle's when setting a Regex for the root node of WorkingPattern.

--- DEiXTo/Views/DeixtoAgentWindow.cs
+++ DEiXTo/Views/DeixtoAgentWindow.cs
@@ -662,8 +662,18 @@ namespace DEiXTo.Views
         /// <param name="node"></param>
         public void UnderlineNode(TreeNode node)
         {
-            var nodeFont = WorkingPatternTreeView.Font;
-            node.NodeFont = new Font(nodeFont, FontStyle.Underline);
+            Font nodeFont;
+            
+            if (node.NodeFont != null)
+            {
+                nodeFont = node.NodeFont;
+            }
+            else
+            {
+                nodeFont = WorkingPatternTreeView.Font;
+            }
+
+            node.NodeFont = new Font(nodeFont, FontStyle.Underline | FontStyle.Bold);
         }
 
         private void BrowseToURLButton_Click(object sender, EventArgs e)
