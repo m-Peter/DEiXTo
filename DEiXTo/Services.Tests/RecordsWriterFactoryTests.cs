4b74e4503f3d436b2c0d7dcfba80553e8402b609
Peter Markou <markoupetr@gmail.com>
2014-10-31 15:37:48 +0200

    Add tests for the RecordsWriterFactory class. Make sure the appropriate writer class is returned for each format.

---
+++ DEiXTo/Services.Tests/RecordsWriterFactoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DEiXTo.Services.Tests
+{
+    [TestClass]
+    public class RecordsWriterFactoryTests
+    {
+        [TestMethod]
+        public void TestReturnsTextWriterForTextFormat()
+        {
+            // Arrange
+            var format = Format.Text;
+            string filename = "some_file";
+
+            // Act
+            var writer = RecordsWriterFactory.GetWriterFor(format, filename);
+
+            // Assert
+            Assert.IsInstanceOfType(writer, typeof(TextRecordsWriter));
+        }
+
+        [TestMethod]
+        public void TestReturnsXmlWriterForXmlFormat()
+        {
+            // Arrange
+            var format = Format.XML;
+            string filename = "some_file";
+
+            // Act
+            var writer = RecordsWriterFactory.GetWriterFor(format, filename);
+
+            // Assert
+            Assert.IsInstanceOfType(writer, typeof(XmlRecordsWriter));
+        }
+    }
+}
