2a65758556955d21c21d6c97e8516f82fd60dc57
Peter Markou <markoupetr@gmail.com>
2014-11-19 13:37:50 +0200

    Remove unused code for ExtractionPatternFileRepository class.

--- DEiXTo/Services/ExtractionPatternFileRepository.cs
+++ DEiXTo/Services/ExtractionPatternFileRepository.cs
@@ -8,24 +8,16 @@ namespace DEiXTo.Services
     public class ExtractionPatternFileRepository : IExtractionPatternRepository
     {
         private readonly string _filename;
-        private readonly IFileLoader _loader;
         private readonly IExtractionPatternMapper _mapper;
 
         public ExtractionPatternFileRepository(string filename)
         {
             _filename = filename;
-            //_loader = loader;
             _mapper = new ExtractionPatternMapper();
         }
 
         public ExtractionPattern Load(Stream stream)
         {
-            /*using (var stream = _loader.Load(_filename, FileMode.Open))
-            {
-                var reader = XmlReader.Create(stream);
-
-                return _mapper.Map(reader);
-            }*/
             var reader = XmlReader.Create(stream);
 
             return _mapper.Map(reader);
@@ -33,13 +25,6 @@ namespace DEiXTo.Services
 
         public void Save(ExtractionPattern pattern, Stream stream)
         {
-            /*using (var stream = _loader.Load(_filename, FileMode.CreateNew))
-            {
-                var writer = new ExtractionPatternWriter();
-
-                writer.Write(stream, pattern);
-            }*/
-
             var writer = new ExtractionPatternWriter();
 
             writer.Write(stream, pattern);
