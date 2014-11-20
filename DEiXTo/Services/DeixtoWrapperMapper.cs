7d4c8752a6c3dd0ddcf608be507c902a3c62461b
Peter Markou <markoupetr@gmail.com>
2014-11-20 12:58:25 +0200

    Refactor the DeixtoWrapperMapper. Introduce helper methods for reading each one of the available elements.

--- DEiXTo/Services/DeixtoWrapperMapper.cs
+++ DEiXTo/Services/DeixtoWrapperMapper.cs
@@ -12,140 +12,202 @@ namespace DEiXTo.Services
         {
             DeixtoWrapper wrapper = new DeixtoWrapper();
 
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.DtdProcessing = DtdProcessing.Parse;
-
             while (reader.Read())
             {
-                if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "InputFile")
+                if (IsElement(reader) && reader.Name == "InputFile")
                 {
-                    reader.MoveToAttribute("Filename");
-                    wrapper.InputFile = reader.Value;
+                    ReadInputFile(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "TargetUrls")
+
+                if (IsElement(reader) && reader.Name == "TargetUrls")
                 {
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(reader.ReadSubtree());
-
-                    int i = 0;
-                    var nodes = doc.ChildNodes[0];
-                    string[] urls = new string[nodes.ChildNodes.Count];
-
-                    foreach (XmlNode node in nodes.ChildNodes)
-                    {
-                        if (node.NodeType == XmlNodeType.Element && node.LocalName == "URL")
-                        {
-                            urls[i] = node.Attributes["Address"].Value;
-                            i++;
-                        }
-                    }
-
-                    wrapper.TargetUrls = urls;
+                    ReadTargetUrls(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "IgnoredTagsList")
+                
+                if (IsElement(reader) && reader.Name == "IgnoredTagsList")
                 {
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(reader.ReadSubtree());
-
-                    int i = 0;
-                    var nodes = doc.ChildNodes[0];
-                    string[] tags = new string[nodes.ChildNodes.Count];
-
-                    foreach (XmlNode node in nodes.ChildNodes)
-                    {
-                        if (node.NodeType == XmlNodeType.Element && node.LocalName == "IgnoredTag")
-                        {
-                            tags[i] = node.Attributes["Label"].Value;
-                            i++;
-                        }
-                    }
-
-                    wrapper.IgnoredTags = tags;
+                    ReadIgnoredTagsList(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "MultiplePage")
+                
+                if (IsElement(reader) && reader.Name == "MultiplePage")
                 {
-                    reader.MoveToAttribute("Enabled");
-                    bool enabled = reader.ReadContentAsBoolean();
-                    reader.MoveToAttribute("ContainsText");
-                    string containsText = reader.Value;
-                    reader.MoveToAttribute("MaxCrawlDepth");
-                    int crawlDepth = reader.ReadContentAsInt();
-                    wrapper.MultiPageCrawling = enabled;
-                    wrapper.HtmlNextLink = containsText;
-                    wrapper.MaxCrawlingDepth = crawlDepth;
+                    ReadMultiplePage(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "OutputFile")
+                
+                if (IsElement(reader) && reader.Name == "OutputFile")
                 {
-                    reader.MoveToAttribute("Filename");
-                    string output_filename = reader.Value;
-                    reader.MoveToAttribute("Format");
-                    var format_value = reader.Value;
-
-                    OutputFormat format = new OutputFormat();
-                    if (format_value == "XML")
-                    {
-                        format = new OutputFormat(Format.XML, format_value);
-                    }
-                    else if (format_value == "TabDelimited")
-                    {
-                        format = new OutputFormat(Format.Text, format_value);
-                    }
-                    else if (format_value == "RSS")
-                    {
-                        format = new OutputFormat(Format.RSS, format_value);
-                    }
-
-                    reader.MoveToAttribute("FileMode");
-                    var mode = reader.Value;
-
-                    wrapper.OutputFileName = output_filename;
-                    wrapper.OutputFormat = format.Format;
-                    wrapper.OutputMode = mode == "Append" ? OutputMode.Append : OutputMode.Overwrite;
+                    ReadOutputFile(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "MaxHits")
+                
+                if (IsElement(reader) && reader.Name == "MaxHits")
                 {
-                    reader.MoveToAttribute("Value");
-                    int maxHits = reader.ReadContentAsInt();
-                    wrapper.NumberOfHits = maxHits;
+                    ReadMaxHits(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "ExtractPageURL")
+                
+                if (IsElement(reader) && reader.LocalName == "ExtractPageURL")
                 {
-                    reader.MoveToAttribute("Enabled");
-                    bool enabled = reader.ReadContentAsBoolean();
-                    wrapper.ExtractNativeUrl = enabled;
+                    ReadExtractPageUrl(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "SubmitForm")
+                
+                if (IsElement(reader) && reader.Name == "SubmitForm")
                 {
-                    reader.MoveToAttribute("Enabled");
-                    bool enabled = reader.ReadContentAsBoolean();
-                    reader.MoveToAttribute("FormName");
-                    string formName = reader.ReadContentAsString();
-                    reader.MoveToAttribute("InputName");
-                    string inputName = reader.ReadContentAsString();
-                    reader.MoveToAttribute("Term");
-                    string term = reader.ReadContentAsString();
-                    wrapper.AutoFill = enabled;
-                    wrapper.FormName = formName;
-                    wrapper.FormInputName = inputName;
-                    wrapper.FormTerm = term;
+                    ReadSubmitForm(reader, wrapper);
                 }
-                else if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "ExtractionPattern")
+                
+                if (IsElement(reader) && reader.Name == "ExtractionPattern")
                 {
-                    var rootNode = new TreeNode();
-                    reader.ReadToFollowing("Node");
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(reader.ReadSubtree());
-                    createPattern(doc.ChildNodes, rootNode);
-                    wrapper.ExtractionPattern = new ExtractionPattern(rootNode);
+                    ReadExtractionPattern(reader, wrapper);
                 }
             }
 
             return wrapper;
         }
 
+        private void ReadInputFile(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            reader.MoveToAttribute("Filename");
+            wrapper.InputFile = reader.Value;
+        }
+
+        private void ReadTargetUrls(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(reader.ReadSubtree());
+
+            int i = 0;
+            var nodes = doc.ChildNodes[0];
+            string[] urls = new string[nodes.ChildNodes.Count];
+
+            foreach (XmlNode node in nodes.ChildNodes)
+            {
+                if (IsElement(node) && node.Name == "URL")
+                {
+                    urls[i] = node.Attributes["Address"].Value;
+                    i++;
+                }
+            }
+
+            wrapper.TargetUrls = urls;
+        }
+
+        private void ReadIgnoredTagsList(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(reader.ReadSubtree());
+
+            int i = 0;
+            var nodes = doc.ChildNodes[0];
+            string[] tags = new string[nodes.ChildNodes.Count];
+
+            foreach (XmlNode node in nodes.ChildNodes)
+            {
+                if (IsElement(node) && node.Name == "IgnoredTag")
+                {
+                    tags[i] = node.Attributes["Label"].Value;
+                    i++;
+                }
+            }
+
+            wrapper.IgnoredTags = tags;
+        }
+
+        private void ReadMultiplePage(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            reader.MoveToAttribute("Enabled");
+            bool enabled = reader.ReadContentAsBoolean();
+            reader.MoveToAttribute("ContainsText");
+            string containsText = reader.Value;
+            reader.MoveToAttribute("MaxCrawlDepth");
+            int crawlDepth = reader.ReadContentAsInt();
+            wrapper.MultiPageCrawling = enabled;
+            wrapper.HtmlNextLink = containsText;
+            wrapper.MaxCrawlingDepth = crawlDepth;
+        }
+
+        private void ReadOutputFile(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            reader.MoveToAttribute("Filename");
+            string output_filename = reader.Value;
+            reader.MoveToAttribute("Format");
+            var format_value = reader.Value;
+
+            OutputFormat format = new OutputFormat();
+            if (format_value == "XML")
+            {
+                format = new OutputFormat(Format.XML, format_value);
+            }
+            
+            if (format_value == "TabDelimited")
+            {
+                format = new OutputFormat(Format.Text, format_value);
+            }
+            
+            if (format_value == "RSS")
+            {
+                format = new OutputFormat(Format.RSS, format_value);
+            }
+
+            reader.MoveToAttribute("FileMode");
+            var mode = reader.Value;
+
+            wrapper.OutputFileName = output_filename;
+            wrapper.OutputFormat = format.Format;
+            wrapper.OutputMode = mode == "Append" ? OutputMode.Append : OutputMode.Overwrite;
+        }
+
+        private void ReadMaxHits(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            reader.MoveToAttribute("Value");
+            int maxHits = reader.ReadContentAsInt();
+            wrapper.NumberOfHits = maxHits;
+        }
+
+        private void ReadExtractPageUrl(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            reader.MoveToAttribute("Enabled");
+            bool enabled = reader.ReadContentAsBoolean();
+            wrapper.ExtractNativeUrl = enabled;
+        }
+
+        private void ReadSubmitForm(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            reader.MoveToAttribute("Enabled");
+            bool enabled = reader.ReadContentAsBoolean();
+            reader.MoveToAttribute("FormName");
+            string formName = reader.ReadContentAsString();
+            reader.MoveToAttribute("InputName");
+            string inputName = reader.ReadContentAsString();
+            reader.MoveToAttribute("Term");
+            string term = reader.ReadContentAsString();
+            wrapper.AutoFill = enabled;
+            wrapper.FormName = formName;
+            wrapper.FormInputName = inputName;
+            wrapper.FormTerm = term;
+        }
+
+        private void ReadExtractionPattern(XmlReader reader, DeixtoWrapper wrapper)
+        {
+            var rootNode = new TreeNode();
+            reader.ReadToFollowing("Node");
+            XmlDocument doc = new XmlDocument();
+            doc.Load(reader.ReadSubtree());
+            createPattern(doc.ChildNodes, rootNode);
+            wrapper.ExtractionPattern = new ExtractionPattern(rootNode);
+        }
+
         private bool NonNodeElement(XmlNode node)
         {
             return (node.NodeType != XmlNodeType.Element) || (node.Name != "Node");
+        }
+
+        private bool IsElement(XmlNode node)
+        {
+            return node.NodeType == XmlNodeType.Element;
+        }
+
+        private bool IsElement(XmlReader reader)
+        {
+            return reader.NodeType == XmlNodeType.Element;
         }
 
         private string ReadTagAttribute(XmlNode node, NodeInfo pInfo, TreeNode treeNode)
