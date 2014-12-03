f20bc508600745dbfc7d796a3bfdef87090232d1
Peter Markou <markoupetr@gmail.com>
2014-12-03 11:01:17 +0200

    Refactor the RegexBuilderPresenterTests class.

--- DEiXTo/Presenters.Tests/RegexBuilderPresenterTests.cs
+++ DEiXTo/Presenters.Tests/RegexBuilderPresenterTests.cs
@@ -11,76 +11,106 @@ namespace DEiXTo.Presenters.Tests
     [TestClass]
     public class RegexBuilderPresenterTests
     {
-        private Mock<IRegexBuilderView> _view;
-        private TreeNode _node;
-        private RegexBuilderPresenter _presenter;
-        private Mock<IEventHub> _eventHub;
+        private Mock<IRegexBuilderView> view;
+        private TreeNode node;
+        private RegexBuilderPresenter presenter;
+        private Mock<IEventHub> eventHub;
 
         [TestInitialize]
         public void SetUp()
         {
-            _view = new Mock<IRegexBuilderView>();
-            _node = new TreeNode("H1");
+            view = new Mock<IRegexBuilderView>();
+            node = new TreeNode("H1");
             NodeInfo nInfo = new NodeInfo();
             nInfo.Content = "This is the header";
-            _node.Tag = nInfo;
-            _eventHub = new Mock<IEventHub>();
-            _presenter = new RegexBuilderPresenter(_view.Object, _node, _eventHub.Object);
+            node.Tag = nInfo;
+            eventHub = new Mock<IEventHub>();
+            presenter = new RegexBuilderPresenter(view.Object, node, eventHub.Object);
         }
 
         [TestMethod]
-        public void TestRegexTextIsPopulatedWithNodesContent()
+        public void TestRegexTextIsPopulatedWithNodeContent()
         {
             // Assert
-            _view.VerifySet(m => m.RegexText = "This is the header");
+            view.VerifySet(v => v.RegexText = "This is the header");
         }
 
         [TestMethod]
         public void TestAddRegex()
         {
             // Arrange
-            _view.Setup(m => m.RegexText).Returns("some regex");
+            view.Setup(v => v.RegexText).Returns("some regex");
 
             // Act
-            _presenter.AddRegex();
+            presenter.AddRegex();
 
             // Assert
-            Assert.AreEqual("some regex", _node.GetRegex());
-            _eventHub.Verify(m => m.Publish(It.IsAny<RegexAdded>()));
-            var nodeFont = _node.NodeFont;
-            Assert.AreEqual(FontStyle.Underline, nodeFont.Style);
-            _view.Verify(m => m.Exit());
+            Assert.AreEqual("some regex", node.GetRegex());
+            eventHub.Verify(e => e.Publish(It.Is<RegexAdded>(ra => ra.Node == node)));
+            view.Verify(v => v.Exit());
         }
 
         [TestMethod]
         public void TestAddInvalidRegex()
         {
             // Arrange
-            _view.Setup(m => m.RegexText).Returns("");
+            view.Setup(v => v.RegexText).Returns("");
 
             // Act
-            _presenter.AddRegex();
+            presenter.AddRegex();
 
             // Assert
-            _view.Verify(m => m.ShowInvalidRegexMessage());
-            _view.Verify(m => m.Exit(), Times.Never);
+            view.Verify(v => v.ShowInvalidRegexMessage());
+            view.Verify(v => v.Exit(), Times.Never);
         }
 
         [TestMethod]
         public void TestPressingEnterAddsRegex()
         {
             // Arrange
-            _view.Setup(m => m.RegexText).Returns("some regex");
+            view.Setup(v => v.RegexText).Returns("some regex");
 
             // Act
-            _presenter.KeyDownPress(Keys.Enter);
+            presenter.KeyDownPress(Keys.Enter);
 
             // Assert
-            Assert.AreEqual("some regex", _node.GetRegex());
-            _eventHub.Verify(m => m.Publish(It.IsAny<RegexAdded>()));
-            var nodeFont = _node.NodeFont;
-            Assert.AreEqual(FontStyle.Underline, nodeFont.Style);
-            _view.Verify(m => m.Exit());
+            Assert.AreEqual("some regex", node.GetRegex());
+            eventHub.Verify(e => e.Publish(It.Is<RegexAdded>(ra => ra.Node == node )));
+            view.Verify(v => v.Exit());
+        }
+
+        [TestMethod]
+        public void TestLoadExistingRegex()
+        {
+            // Arrange
+            node.SetRegex("[0-9]{2}");
+            node.SetInverse(true);
+
+            // Act
+            presenter = new RegexBuilderPresenter(view.Object, node, eventHub.Object);
+
+            // Assert
+            view.VerifySet(v => v.RegexText = "[0-9]{2}");
+            view.VerifySet(v => v.InverseRegex = true);
+        }
+
+        [TestMethod]
+        public void TestChangeExistingRegex()
+        {
+            // Arrange
+            node.SetRegex("[a-z]{4}");
+            node.SetInverse(false);
+
+            // Act
+            presenter = new RegexBuilderPresenter(view.Object, node, eventHub.Object);
+            view.Setup(v => v.RegexText).Returns("[0-9]{2}");
+            view.Setup(v => v.InverseRegex).Returns(true);
+            presenter.AddRegex();
+
+            // Assert
+            Assert.AreEqual("[0-9]{2}", node.GetRegex());
+            eventHub.Verify(e => e.Publish(It.Is<RegexAdded>(ra => ra.Node == node)));
+            view.Verify(v => v.Exit());
         }
     }
 }
