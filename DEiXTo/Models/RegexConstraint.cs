3d12184938a1e59bc614edb7cf0742f522b023c7
Peter Markou <markoupetr@gmail.com>
2014-12-14 11:50:21 +0200

    Implement a RegexConstraint class that will evaluate and expose the value of a regular expression constraint.

---
+++ DEiXTo/Models/RegexConstraint.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DEiXTo.Models
+{
+    public enum ConstraintAction
+    {
+        Match,
+        MatchAndExtract
+    }
+
+    public class RegexConstraint
+    {
+        private string _input;
+        private string _pattern;
+        private string _value;
+        private ConstraintAction _action;
+
+        public RegexConstraint(string input, string pattern, ConstraintAction action = ConstraintAction.MatchAndExtract)
+        {
+            _input = input;
+            _pattern = pattern;
+            _value = String.Empty;
+            _action = action;
+        }
+
+        public string Input
+        {
+            get { return _input; }
+        }
+
+        public string Pattern
+        {
+            get { return _pattern; }
+        }
+
+        public bool Evaluate()
+        {
+            var match = Regex.Match(_input, _pattern);
+
+            if (!match.Success)
+            {
+                return false;
+            }
+
+            var sb = new StringBuilder();
+            sb.Append(match.Value);
+            while (match.Success)
+            {
+                match = match.NextMatch();
+                sb.Append(" " + match.Value);
+            }
+
+            _value = sb.ToString(0, sb.Length - 1);
+            return true;
+        }
+
+        public string Value
+        {
+            get
+            {
+                if (_action == ConstraintAction.Match)
+                {
+                    return _input;
+                }
+
+                return _value;
+            }
+        }
+    }
+}
---
+++ DEiXTo/Services/RegexContraintTests.cs
@@ -0,0 +1,90 @@
+﻿using DEiXTo.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DEiXTo.Services
+{
+    [TestClass]
+    public class RegexContraintTests
+    {
+        [TestMethod]
+        public void TestCreateNewRegexConstraint()
+        {
+            // Arrange
+            var input = "[#1] International Trade";
+            var pattern = @"\d+";
+
+            // Act
+            var constraint = new RegexConstraint(input, pattern);
+
+            // Assert
+            Assert.AreEqual(input, constraint.Input);
+            Assert.AreEqual(pattern, constraint.Pattern);
+        }
+
+        [TestMethod]
+        public void TestEvaluateRegexConstraint()
+        {
+            // Arrange
+            var input = "[#1] International Trade";
+            var pattern = @"\d+";
+
+            // Act
+            var constraint = new RegexConstraint(input, pattern);
+
+            // Assert
+            Assert.IsTrue(constraint.Evaluate());
+        }
+
+        [TestMethod]
+        public void TestDoesNotEvaluateRegexConstraint()
+        {
+            // Arrange
+            var input = "International Trade";
+            var pattern = @"\d+";
+
+            // Act
+            var constraint = new RegexConstraint(input, pattern);
+
+            // Assert
+            Assert.IsFalse(constraint.Evaluate());
+        }
+
+        [TestMethod]
+        public void TestMatchContentFromRegexConstraint()
+        {
+            // Arrange
+            var input = "[#1] International Trade";
+            var pattern = @"\d+";
+
+            // Act
+            var constraint = new RegexConstraint(input, pattern, ConstraintAction.Match);
+            constraint.Evaluate();
+
+            // Assert
+            Assert.AreEqual("[#1] International Trade", constraint.Value);
+        }
+
+        [TestMethod]
+        public void TestMatchAndExtractContentFromRegexConstraint()
+        {
+            // Arrange
+            var input = "[#1] International Trade";
+            var pattern = "[a-zA-Z]+";
+
+            // Act
+            var constraint = new RegexConstraint(input, pattern);
+            constraint.Evaluate();
+
+            // Assert
+            var match = Regex.Match(input, pattern);
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual("International", match.Value);
+            var nextMatch = match.NextMatch();
+            Assert.AreEqual("Trade", nextMatch.Value);
+            Assert.AreEqual("International Trade", constraint.Value);
+        }
+    }
+}
